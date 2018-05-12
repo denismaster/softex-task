c4a0d459aa96ff80c53bfac227a531a497c10a31
Denis Obolensky <denis_obolensky@xrm.ru>
2018-05-12 10:14:53 +0300

    Style Header and make it sticky

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,32 @@
 <template>
-    <div id="nav">
-        <router-link to="/">Home</router-link>
-        <router-link to="/about">About</router-link>
-        <router-link to="/contacts">Contacts</router-link>
-    </div>
+    <nav class="header header-sticky header-centered navbar navbar-expand-lg navbar-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <router-link class="nav-item nav-link" to="/">Home</router-link>
+                <router-link class="nav-item nav-link" to="/about">About</router-link>
+                <router-link class="nav-item nav-link" to="/contacts">Contacts</router-link>
+            </div>
+        </div>
+    </nav>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  name: 'Header'
+});
+</script>
+
+<style>
+.header {
+    background-color: white !important;
+}
+
+.header.header-sticky{
+    position: sticky;
+    top:0;
+}
+</style>
